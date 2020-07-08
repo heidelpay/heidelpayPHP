97485fb4b800c718be555c106167aef177c77576
David.Owusu <david.owusu@heidelpay.de>
2020-07-08 10:42:21 +0200

    [change] PHPLIB-301 - Add payment type constant.

--- src/Constants/IdStrings.php
+++ src/Constants/IdStrings.php
@@ -52,6 +52,7 @@ class IdStrings
     public const INVOICE_FACTORING = 'ivf';
     public const HIRE_PURCHASE_DIRECT_DEBIT = 'hdd';
     public const PAYMENT_PAGE = 'ppg';
+    public const BANCONTACT = 'bct';
 
     // Resources
     public const BASKET = 'bsk';
