9f78521b824d90bcce1cbbc0ed4796ede5ceb3b0
David.Owusu <david.owusu@heidelpay.de>
2020-07-08 10:43:45 +0200

    [change] PHPLIB-301 - Add Bancontact authorize test.

--- test/integration/PaymentTypes/BancontactTest.php
+++ test/integration/PaymentTypes/BancontactTest.php
@@ -2,6 +2,8 @@
 
 namespace heidelpayPHP\test\integration\PaymentTypes;
 
+use heidelpayPHP\Constants\ApiResponseCodes;
+use heidelpayPHP\Exceptions\HeidelpayApiException;
 use heidelpayPHP\Resources\PaymentTypes\Bancontact;
 use heidelpayPHP\test\BasePaymentTest;
 
@@ -22,5 +24,19 @@ class BancontactTest extends BasePaymentTest
 
         $this->heidelpay->fetchPaymentType($bancontact->getId());
         $this->assertInstanceOf(Bancontact::class, $bancontact);
+    }
+
+    /**
+     * Verify that an exception is thrown when giropay authorize is called.
+     *
+     * @test
+     */
+    public function bancontactShouldThrowExceptionOnAuthorize()
+    {
+        $this->expectException(HeidelpayApiException::class);
+        $this->expectExceptionCod(ApiResponseCodes::API_ERROR_TRANSACTION_AUTHORIZE_NOT_ALLOWED);
+
+        $bancontact = $this->heidelpay->createPaymentType(new Bancontact());
+        $this->heidelpay->authorize(100.0, 'EUR', $bancontact, self::RETURN_URL);
     }
 }
